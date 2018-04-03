5f35ec50f964a4455e9dbbefbf0320580e4ac396
Sachinkumar234 <33681546+Sachinkumar234@users.noreply.github.com>
2018-04-03 23:49:43 +0530

    Update program.c

--- program.c
+++ program.c
@@ -1,4 +1,3 @@
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -14,45 +13,67 @@ int data=5;
 
 void *writer(void *arg)
 {
-
-do
-{
 	pthread_mutex_lock(&rw_mutex);
+	
 	printf("Writer writing data...\n");
 	data=data+10;
+	sleep(2);
 	printf("Writed data:%d\n",data);
+	
 	pthread_mutex_unlock(&rw_mutex);
-}while(1);
-
 }
+
 
 void *reader(void *arg1)
 {
-do
-{
+	printf("Reader trying to read data\n");
+	
 	pthread_mutex_lock(&mutex);
+	
 	read_count++;
 
 	if(read_count==1)
 	{
 		pthread_mutex_lock(&rw_mutex);
 	}
+	
 	pthread_mutex_unlock(&mutex);
 
 	printf("Reader reading data...\n");
+	sleep(2);
 	printf("Readed data:%d\n",data);
 
 	pthread_mutex_lock(&mutex);
+	
 	read_count--;
+	
 	if(read_count==0)
 	{
 		pthread_mutex_unlock(&rw_mutex);
 	}
+	
 	pthread_mutex_unlock(&mutex);
-}while(1);
 }
 
 int main()
 {
-
+	pthread_t read[5];
+	pthread_t write[5];
+	
+	pthread_mutex_init(&rw_mutex,NULL);
+	pthread_mutex_init(&mutex,NULL);
+	
+	int i;
+	
+	for(i=0;i<5;i++)
+	{
+		pthread_create(&write[i],NULL,&writer,NULL);
+		pthread_create(&read[i],NULL,&reader,NULL);
+	}
+	
+	for(i=0;i<5;i++)
+	{
+		pthread_join(write[i],NULL);
+    		pthread_join(read[i],NULL);
+	}
 }
